3752f7955b7ad5bce981138b84931950298d2a18
Pavel <sivec_p@mail.ru>
2023-01-16 00:38:16 +0300

    Add a Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+Console.WriteLine("третьей цифры нет");
+else if (100 <= num & num < 1000)
+Console.WriteLine(num % 10);
+else
+{
+while (num >= 1000)
+    num /= 10;        
+Console.WriteLine(num % 10);
+}
+ 
+
