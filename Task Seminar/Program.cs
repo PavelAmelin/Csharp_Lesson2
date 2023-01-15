1bc9c03eff31a6ec7dcf0e4aafe680301d789aa3
Pavel <sivec_p@mail.ru>
2023-01-16 00:40:15 +0300

    Add a Task Seminar

---
+++ Task Seminar/Program.cs
@@ -0,0 +1,17 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int x3 = Convert.ToInt32(Console.ReadLine());
+int x4 = Convert.ToInt32(Console.ReadLine());
+if (x3 > x2 || x1 > x4)
+    Console.WriteLine("пустое множество");
+if (x1 > x3)
+    x3 = x1;
+if (x2 > x4)
+    x2 = x4;
+if (x2 == x3)
+    Console.WriteLine(x2);
+else
+{
+    Console.Write($"{x3} ");
+    Console.WriteLine(x2);
+}
